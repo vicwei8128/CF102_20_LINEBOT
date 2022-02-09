31979ae3855a2421226941e45c8029a4204d0438
vicwei8128 <vic8736@gmail.com>
2022-02-10 01:46:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -176,12 +176,12 @@ def getNameEmojiMessage():
     "emojis": [
       {
         "index": 0,
-        "productId": "5ac1bfd5040ab15980c9b435",
+        "productId": "5ac21a8c040ab15980c9b43f",
         "emojiId": "001"
       },
       {
         "index": 13,
-        "productId": "5ac1bfd5040ab15980c9b435",
+        "productId": "5ac21a8c040ab15980c9b43f",
         "emojiId": "002"
       }
     ]
