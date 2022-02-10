77dc5f304fb680e214ccbd75bb36e91c7920575d
vicwei8128 <vic8736@gmail.com>
2022-02-10 19:27:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -190,37 +190,6 @@ def getNameEmojiMessage():
 
 
 def getCarouselMessage(data):
-    message = dict()
-    return message
-
-
-def getLocationConfirmMessage(title, latitude, longitude):
-    data = {'title': title, 'latitude': latitude, 'longitude': longitude, "action": "get_near"}
-    message = {
-      "type": "template",
-      "altText": "this is a confirm template",
-      "template": {
-          "type": "confirm",
-          "text": f"確認是否搜尋 {title} 附近地點",
-          "actions": [
-               {
-                "type": "postback",
-                "label": "是",
-                "data": json.dumps(data)
-             },
-              {
-                "type": "message",
-                "label": "否",
-                "text": "否"
-              }
-              ]
-      }
-    }
-    
-    return message
-
-
-def getCallCarMessage(data):
     message = {
       "type": "template",
       "altText": "this is a image carousel template",
@@ -254,6 +223,38 @@ def getCallCarMessage(data):
         ]
         }
     }
+    return message
+
+
+def getLocationConfirmMessage(title, latitude, longitude):
+    data = {'title': title, 'latitude': latitude, 'longitude': longitude, "action": "get_near"}
+    message = {
+      "type": "template",
+      "altText": "this is a confirm template",
+      "template": {
+          "type": "confirm",
+          "text": f"確認是否搜尋 {title} 附近地點",
+          "actions": [
+               {
+                "type": "postback",
+                "label": "是",
+                "data": json.dumps(data)
+             },
+              {
+                "type": "message",
+                "label": "否",
+                "text": "否"
+              }
+              ]
+      }
+    }
+    
+    return message
+
+
+def getCallCarMessage(data):
+    message = dict()
+
     return message
 
 
