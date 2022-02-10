66eea6191ad6542debd9d6e193c8b56c1ee7612f
vicwei8128 <vic8736@gmail.com>
2022-02-10 14:12:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -186,8 +186,20 @@ def sendTextMessageToMe():
 def getNameEmojiMessage():
     lookUpStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     productId = "5ac21a8c040ab15980c9b43f"
-    name = ""
+    name = "Vic"
     message = dict()
+    message["type"] = "text"
+    message["text"] = "".join("$" for r in range(len(name)))
+    emojis_list = list()
+    for i, nChar in enumerate(name):
+        emojis_list.append(
+            {
+                "index": i,
+                "productId": productId
+                "emojiID": f"{lookUpStr.index(nChar) + 1 :03}"
+            }
+        )
+    message["emojis"] = emojis_list
     print(message)
     return message
 
