c01923b5938d1d69e306a389fe80f287a10b7db5
vicwei8128 <vic8736@gmail.com>
2022-02-10 20:03:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -201,7 +201,7 @@ def getCarouselMessage(data):
                 "action": {
                   "type": "postback",
                   "label": "台北101",
-                  "data": "action=buy&itemid=111"
+                  "data": json.dumps(data)
                 }
               },
               {
@@ -209,7 +209,7 @@ def getCarouselMessage(data):
                 "action": {
                 "type": "postback",
                 "label": "台北101",
-                "data": "action=buy&itemid=111"
+                "data": json.dumps(data)
                }
               },
             #   {
@@ -253,8 +253,22 @@ def getLocationConfirmMessage(title, latitude, longitude):
 
 
 def getCallCarMessage(data):
-    message = dict()
-
+    message = {
+      "type": "template",
+      "altText": "this is a confirm template",
+      "template": {
+          "type": "buttons",
+          "text": f"請選擇 {data['title']} 預約叫車時間",
+          "actions": [
+               {
+                "type": "datetimepicker",
+                "label": "預約",
+                "data": json.dumps(data)
+                "mode": "datetime"
+            }
+            ]
+      }
+    }
     return message
 
 
